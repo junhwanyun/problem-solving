965a5ed2ef7d052ff2105b4991e786a3becc26cd
junhwanyun <junhwanyun9356@gmail.com>
2020-05-25 21:17:55 +0900

    Sol: 4579. 세상의 모든 팰린드롬 2

---
+++ sw-expert-academy/4579. 세상의 모든 팰린드롬 2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int T;
+string result;
+
+int main() {
+  cin >> T;
+
+  for(int t = 1; t <= T; t++) {
+    result = "";
+
+    string str;
+      
+    cin >> str;
+
+    for(int i = 0; i < str.length(); i++) {
+      if(str[i] == '*' || str[str.length() - 1 - i] == '*') {
+        result = "Exist";
+
+        break;
+      }
+
+      if(str[i] != str[str.length() - 1 - i]) {
+        result = "Not exist";
+
+        break;
+      }
+    }
+
+    if(result == "") {
+      result = "Exist";
+    }
+
+    cout << '#' << t << ' ' << result << '\n';
+  }
+
+  return 0;
+}
